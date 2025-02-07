a17621a174792a072b9e2150fc932e54c3195860
cesarbaquiro <cmbm.kf@gmail.com>
2025-02-07 12:38:40 -0500

    Add validations for creation form

--- src/app/components/form-create-vehicle/form-create-vehicle.component.ts
+++ src/app/components/form-create-vehicle/form-create-vehicle.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { VehicleService } from '../../core/services/vehicle.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-create-vehicle',
@@ -9,37 +10,57 @@ import { VehicleService } from '../../core/services/vehicle.service';
   templateUrl: './form-create-vehicle.component.html',
   styleUrl: './form-create-vehicle.component.css'
 })
-export class FormCreateVehicleComponent {
+export class FormCreateVehicleComponent implements OnInit {
   formCreateVehicle!: FormGroup;
   availableColors: string[] = ['Red', 'Blue', 'Green', 'Yellow', 'Black', 'White', 'Silver', 'Orange', 'Purple', 'Brown'];
+  plateExists = false;
+
 
   constructor(private vehicleService: VehicleService, private fb: FormBuilder) {
     this.formCreateVehicle = this.fb.group({
-      plate: [''],
-      brand: [''],
-      model: [''],
+      plate: ['', Validators.required],
+      brand: ['', Validators.required],
+      model: ['', Validators.required],
       series: [''],
-      model_year: [''],
-      color: ['']
+      model_year: ['', Validators.required],
+      color: ['', Validators.required],
     });
   }
 
-
-  createNewVehicle(): void {
-
-    console.log('Form values:', this.formCreateVehicle.value);
-
-    this.vehicleService.createNewVehicle(this.formCreateVehicle.value).subscribe(
-      data => {
-        console.log('Vehicle created!', data);
-        this.formCreateVehicle.reset();
-      },
-      error => {
-        console.error('There was an error creating the vehicle!', error);
-      }
-    );
-
+  ngOnInit() {
+    // Escuchar cambios en el campo "plate"
+    this.formCreateVehicle.get('plate')?.valueChanges.subscribe(value => {
+      this.vehicleService.verifyPlateExists(value).subscribe((data) => {
+        this.plateExists = data.exists;
+      });
+    });
   }
 
+  createNewVehicle(): void {
+    if (this.formCreateVehicle.valid && !this.plateExists) {
+      this.vehicleService.createNewVehicle(this.formCreateVehicle.value).subscribe(
+        data => {
+          Swal.fire({
+            title: "Vehicle created!",
+            icon: "success",
+            draggable: true
+          });
+          this.formCreateVehicle.reset();
+        },
+        error => {
+          console.error('There was an error creating the vehicle!', error);
+        }
+      );
+
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: 'Fill in the required fields'
+      });
+    }
+
+  }
 
 }
